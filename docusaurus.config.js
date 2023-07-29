617afb8937b54c0e2a85aeff9b71cd26d8c77323
Andrei Surzhan <surzhan.a.y@gmail.com>
2023-07-28 23:37:06 -0400

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AndreySurzhan', // Usually your GitHub org/user name.
-  projectName: 'andrei-builds-software.github.io', // Usually your repo name.
+  projectName: 'andrei-builds-software', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

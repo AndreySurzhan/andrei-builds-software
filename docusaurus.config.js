69e54e770cc7584124e66a3aba3dd6b7ff956572
Andrei Surzhan <surzhan.a.y@gmail.com>
2023-07-28 23:53:20 -0400

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Hello',
+  title: 'Andrei Builds Software',
   tagline: 'Andrei Builds Software',
   favicon: 'img/favicon.ico',
 
